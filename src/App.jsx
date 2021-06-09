7782703cef4c083e399b43247743f125bd51428b
asalex04 <arc04@mail.ru>
2021-06-09 14:01:45 +0400

    modified:   .eslintrc.yml 	modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,38 @@ import React from 'react';
 import {
   Link, BrowserRouter as Router, Route, Switch,
 } from 'react-router-dom';
+import {
+  Button, Navbar, Nav, Container,
+} from 'react-bootstrap';
 import Channels from './components/Channels.jsx';
 import Form from './components/Form.jsx';
 
-const NoMatchPage = () => (<h3>404 - Not found</h3>);
+const NoMatchPage = () => { <h3>404 - Not found</h3>; };
 
-const App = () => (
-  <section className="App">
-    <nav className="mb-3 navbar navbar-expand-lg navbar-light bg-light">
-      <a className="mr-auto navbar-brand" href="/">Hexlet Chat</a>
-      <button type="button" className="btn btn-primary">Выйти</button>
-    </nav>
-    <Router>
-      <Link to="/login">Login</Link>
-      <Link to="/">Home</Link>
-      <Switch>
-        <Route exact path="/" component={Channels} />
-        <Route path="/login" component={Form} />
-        <Route path="*">
-          <NoMatchPage />
-        </Route>
-      </Switch>
-    </Router>
-  </section>
-);
+const App = () => {
+  return (
+    <>
+      <Router>
+        <Container>
+          <Navbar bg="dark" variant="dark" expand="true" gy-5>
+            <Navbar.Brand as={Link} to="/">Hexlet Chat</Navbar.Brand>
+            <Nav className="justify-content-end">
+              <Button variant="primary">Выйти</Button>
+            </Nav>
+          </Navbar>
+        </Container>
+        <Container className="pt-5">
+          <Switch>
+            <Route exact path="/" component={Channels} />
+            <Route path="/login" component={Form} />
+            <Route path="*">
+              <NoMatchPage />
+            </Route>
+          </Switch>
+        </Container>
+      </Router>
+    </>
+  );
+};
 
 export default App;
